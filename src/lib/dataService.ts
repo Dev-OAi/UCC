c998249c1b9466b12f9334573e47d25b771e8c08
Dev-OAi <oleg.m.menyuk@gmail.com>
2026-02-15 23:38:56 -0500

    Rename 'Expiration Date' to 'Expires'

--- src/lib/dataService.ts
+++ src/lib/dataService.ts
@@ -96,7 +96,7 @@ export async function loadCsv(file: FileManifest): Promise<DataRow[]> {
             4: 'Sunbiz Link',
             41: 'Status',                    // Column AP
             42: 'Date Filed',               // Column AQ
-            43: 'Expiration Date',          // Column AR
+            43: 'Expires',                  // Column AR
             44: 'Filings Completed Through', // Column AS
             45: 'Summary For Filing',       // Column AT
             55: 'Florida UCC Link'          // Column BD
