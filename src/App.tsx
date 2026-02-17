74a311904c11d8557db0b46c84536449f644e8cc
Dev-OAi <oleg.m.menyuk@gmail.com>
2026-02-16 22:22:35 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {
   DataRow, 
   isZipCode, 
   isPhoneNumber 
-} from './services/dataService';
+} from './lib/dataService';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Dashboard from './components/Dashboard';
